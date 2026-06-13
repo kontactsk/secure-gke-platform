1316954aa389a14b739b8c48c7a3fff7215aa0dc
skiran12345 <kontactsaikiran@gmail.com>
2026-06-12 20:40:53 -0400

    style: apply terraform fmt across all modules

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -6,13 +6,13 @@
 
 
 resource "google_service_account" "this" {
- project      = var.project_id
- account_id   = var.service_account_name
- display_name = "Managed by Terraform - ${var.service_account_name}"
+  project      = var.project_id
+  account_id   = var.service_account_name
+  display_name = "Managed by Terraform - ${var.service_account_name}"
 }
 
 resource "google_project_iam_member" "this" {
- project      = var.project_id
- role         = var.role
- member       = "serviceAccount:${google_service_account.this.email}"
- }
+  project = var.project_id
+  role    = var.role
+  member  = "serviceAccount:${google_service_account.this.email}"
+}
